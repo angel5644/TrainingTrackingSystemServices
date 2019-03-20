550b7ce4e219f84c8bfd6444f2daaedf1a81fc4a
LuisAlbertoRoblesHer <lrobles@4thsource.com>
2019-03-20 12:34:11 -0600

    Created the user's model and the method to create the user and save it to the database using the user's service. Still not tested because the database is not created yet.

--- User Management/src/main/java/com/usermanagement/model/User.java
+++ User Management/src/main/java/com/usermanagement/model/User.java
@@ -1,11 +1,74 @@
 package com.usermanagement.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "USERS", catalog = "XE")
 public class User {
-	//aber
 
+	@Id
+	@Column(name = "id",length=32, unique = true, nullable = false, insertable = false, updatable = false)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQUENCE_NAME")
+	@SequenceGenerator(name = "SEQUENCE_NAME", sequenceName = "SEQUENCE_NAME", allocationSize = 1, initialValue = 1)
+	private Integer id;
+	@Column(name = "first_name",nullable = false, length = 50)
+	private String first_name;
+	@Column(name = "last_name",nullable = false, length = 50)
+	private String last_name;
+	@Column(name = "email",nullable = false, length = 50)
+	private String email;
+	@Column(name = "type",nullable = false, length =4 )
+	private Integer type;
+	
+	public User(){}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getType() {
+		return type;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	
+	
+	
 }
--- User Management/src/main/java/com/usermanagement/resource/UserResource.java
+++ User Management/src/main/java/com/usermanagement/resource/UserResource.java
@@ -1,9 +1,15 @@
 package com.usermanagement.resource;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.usermanagement.model.User;
 import com.usermanagement.repository.UserRepository;
 
 @RestController
@@ -12,4 +18,31 @@ public class UserResource {
 
 	@Autowired
     UserRepository userRepository;
+	
+	@RequestMapping(value= "/create_user_post", method = RequestMethod.POST)
+	@ResponseBody
+	public String createCustomer(HttpServletRequest request){
+		
+		//String id = request.getParameter("id");
+		String first_name= request.getParameter("first_name");
+		String last_name= request.getParameter("last_name");
+		String email= request.getParameter("email");
+		String type= request.getParameter("type");
+		
+		String result="";
+
+	    try{
+	        User u = new User();
+	        u.setFirst_name(first_name);
+	        u.setLast_name(last_name);
+	        u.setEmail(email);
+	        u.setType(Integer.parseInt(type));
+	        
+	        userRepository.save(u);
+	        result = "Insert success";
+	    }catch(Exception ex){
+	        System.out.println(ex);
+	    }
+	    return result;
+	}
 }
