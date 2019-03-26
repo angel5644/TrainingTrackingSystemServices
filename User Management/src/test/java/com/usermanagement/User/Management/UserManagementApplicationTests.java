66fe538164e61853d5690b783b20d4e4e8fff1e9
Rodrigo Velasco Rivas <rodrigo.velasco@4thsource.com>
2019-03-26 08:17:29 -0600

    Unit test for CreateUser method

--- User Management/src/test/java/com/usermanagement/User/Management/UserManagementApplicationTests.java
+++ User Management/src/test/java/com/usermanagement/User/Management/UserManagementApplicationTests.java
@@ -1,28 +1,19 @@
 package com.usermanagement.User.Management;
 
 import static org.junit.Assert.assertEquals;
-
-import java.io.UnsupportedEncodingException;
-
-import javax.transaction.Transactional;
-
-import org.aspectj.lang.annotation.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.usermanagement.UserManagementApplication;
+import com.usermanagement.model.User;
 import com.usermanagement.resource.UserResource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserManagementApplicationTests {
+	@Autowired
 	
 	@Mock
 	UserResource userResource;
@@ -35,32 +26,12 @@ public class UserManagementApplicationTests {
 		String email = "rodrigo.velasco@4thsource.com";
 		int type = 0;
 		
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		request.setContextPath("http://localhost:9091/user/create_user_post");
-		request.setMethod("POST");
-		request.setContentType("text/plain");
-		request.setCharacterEncoding("UTF-8");
-		request.setParameter("first_name", first_name);
-		request.setParameter("last_name", last_name);
-		request.setParameter("email", email);
-		request.setParameter("type", String.valueOf(type));
+		User user = new User();
+		user.setFirst_name(first_name);
+		user.setLast_name(last_name);
+		user.setEmail(email);
+		user.setType(type);
 		
-		try
-		{
-			System.out.println("ABERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR" + request.getContentAsString());
-		}
-		catch (IllegalStateException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		catch (UnsupportedEncodingException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		//System.out.println("Resultado = " + userResource.createUser(request));
-		//assertEquals("Insert success", userResource.createUser(request));      
+		assertEquals("Insert success", userResource.createUser(user));      
     }
 }
