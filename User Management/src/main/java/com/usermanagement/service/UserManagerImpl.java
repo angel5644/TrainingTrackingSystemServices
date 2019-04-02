f06df2d7018a549c8ec6aa5a1fde6586ea352549
Erick-AC <eavila4@ucol.mx>
2019-04-02 12:49:44 -0600

    ShowUser

--- User Management/src/main/java/com/usermanagement/service/UserManagerImpl.java
+++ User Management/src/main/java/com/usermanagement/service/UserManagerImpl.java
@@ -75,6 +75,11 @@ public class UserManagerImpl implements UserManager {
     }
 	
 	@Override
+    @Transactional
+	public List<Users> viewUser(String id){
+			return (userRepository.findById(Integer.valueOf(id)) == null)? null: userRepository.findById((long)Integer.valueOf(id));
+	}
+	@Override
 	@Transactional
 	public FindUserResponse findUsers(String searchField,String searchValue,String orderBy,String orderType,Integer pageNo,Integer numberRec){
 		
