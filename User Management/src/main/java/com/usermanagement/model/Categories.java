da7024caf41d83acc0da1b30a05e0de16ff10b73
LuisAlbertoRoblesHer <lrobles@4thsource.com>
2019-04-15 12:34:37 -0500

    Added categories model for list cateogires module

---
+++ User Management/src/main/java/com/usermanagement/model/Categories.java
@@ -0,0 +1,31 @@
+ package com.usermanagement.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "CATEGORIES", catalog = "XE")
+@Data
+public class Categories {
+	
+	@Id
+	@Column(name = "id",length=32, unique = true, nullable = false, insertable = false, updatable = false)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQUENCE_NAME_CATEGORY")
+	@SequenceGenerator(name = "SEQUENCE_NAME_CATEGORY", sequenceName = "SEQUENCE_NAME_CATEGORY", allocationSize = 1, initialValue = 1)
+	private Integer id;
+	@Column(name = "name",nullable = false, length = 50, unique=true)
+	private String name;
+	@Column(name = "description",nullable = false, length = 500)
+	private String description;
+	
+	public Categories(){}
+
+
+}
