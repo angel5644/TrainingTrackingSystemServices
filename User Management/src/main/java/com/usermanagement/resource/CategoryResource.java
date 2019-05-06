4e20a683f2eed01bb3fe94dc6850d44cd914c0e9
LuisAlbertoRoblesHer <lrobles@4thsource.com>
2019-05-06 08:04:38 -0500

    Added Cross Origin anotation to allow external calls

--- User Management/src/main/java/com/usermanagement/resource/CategoryResource.java
+++ User Management/src/main/java/com/usermanagement/resource/CategoryResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.usermanagement.model.Categories;
 import com.usermanagement.service.CategoryManager;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class CategoryResource {
 
