4130fb72360aa9c19e61e3f98abafc483146d54e
LuisAlbertoRoblesHer <lrobles@4thsource.com>
2019-05-07 12:48:45 -0500

    Added model layer for Courses

---
+++ User Management/src/main/java/com/usermanagement/model/Courses.java
@@ -0,0 +1,34 @@
+package com.usermanagement.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "COURSES", catalog = "XE")
+@Data
+public class Courses {
+
+	@Id
+	@Column(name = "id",length=32, unique = true, nullable = false, insertable = false, updatable = false)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQUENCE_NAME_CATEGORY")
+	@SequenceGenerator(name = "SEQUENCE_NAME_CATEGORY", sequenceName = "SEQUENCE_NAME_CATEGORY", allocationSize = 1, initialValue = 1)
+	private Integer id;
+	@Column(name = "name",nullable = false, length = 50, unique=true)
+	private String name;
+	@Column(name = "description",nullable = true, length = 500)
+	private String description;
+	@Column(name = "categories",nullable = false, length = 32)
+	private Integer categories;
+	@Column(name = "content",nullable = false, length = 4000)
+	private String content;
+	
+	public Courses(){}
+	
+}
